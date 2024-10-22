5793c214862995bd1ab2e60c03f2d0c74411aeb3
MariaClaraOliveira01 <165787108+MariaClaraOliveira01@users.noreply.github.com>
2024-10-22 09:10:56 -0300

    oi

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let valorContador = 0;
 function incrementar(){
     valorContador++;
-    document.getElementByld('contador').innerText= valorContador;
+    document.getElementByld('contador').innerText = valorContador;
 }
 
 function diminuir(){
     valorContador--;
-    document.getElementByld('contador').innerText= valorContador;
+    document.getElementByld('contador').innerText = valorContador;
 }
